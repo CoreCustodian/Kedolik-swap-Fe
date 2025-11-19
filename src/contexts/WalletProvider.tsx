8545e742ccfa0ea494865d204db68b466936fce6
Muhammad Shahrukh Azeem <89108936+Muhammad-Shahrukh-Azeem@users.noreply.github.com>
2025-11-19 17:48:59 +0500

    rpc updsated

--- src/contexts/WalletProvider.tsx
+++ src/contexts/WalletProvider.tsx
@@ -3,7 +3,6 @@ import { ConnectionProvider, WalletProvider as SolanaWalletProvider } from '@sol
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
